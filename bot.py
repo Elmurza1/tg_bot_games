5b38fee81572d8fa161b334365b3ac9e988bfe3e
Elmurza1 <elmyrza217@mail.com>
2024-05-11 19:14:22 +0600

    teletele_bot

---
+++ bot.py
@@ -0,0 +1,14 @@
+import telebot
+from local_setting import API_TOKEN
+
+bot = telebot.TeleBot(API_TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.infinity_polling()
